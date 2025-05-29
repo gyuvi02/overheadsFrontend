66f41d4dd636c7b82b34b3c3734a77d2998db021
Gyula Szabo <gyulaother@gmail.com>
2025-05-29 10:38:14 +0200

    Properties updated

--- src/environments/environments.prod.ts
+++
@@ -1,8 +0,0 @@
-
-export const environment = {
-  production: true,
-  appName: 'Overheads Management',
-  defaultLanguage: 'en',
-  apiBaseUrl: 'https://www.omegahouses.org/api/v1',
-  apiKeyValid: 'z$T9!s#K2@p7*qR5&uX3^wE1'
-};
