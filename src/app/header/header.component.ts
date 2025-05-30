9432373f02e952a6dbcaa25f8d8931c741ae9031
Gyula Szabo <gyulaother@gmail.com>
2025-05-30 08:16:31 +0200

    Added new translations to the i18n files and added a new button to the header to switch between languages.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,9 @@
-import { Component, inject } from '@angular/core';
-import { ButtonComponent } from "../shared/button/button.component";
-import { Router, NavigationEnd } from '@angular/router';
-import { filter } from 'rxjs/operators';
-import { NgIf, AsyncPipe } from '@angular/common';
-import { AuthService } from '../core/auth.service';
+import {Component, inject} from '@angular/core';
+import {ButtonComponent} from "../shared/button/button.component";
+import {NavigationEnd, Router} from '@angular/router';
+import {filter} from 'rxjs/operators';
+import {AsyncPipe, NgIf} from '@angular/common';
+import {AuthService} from '../core/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -18,10 +18,13 @@ import { AuthService } from '../core/auth.service';
 })
 export class HeaderComponent {
   currentRoute: string = '';
+  currentLang: 'en' | 'hu' = 'hu'; // Default to Hungarian
   private authService = inject(AuthService);
   isLoggedIn$ = this.authService.isLoggedIn$;
 
   constructor(private router: Router) {
+    this.detectLangFromUrl();
+
     this.router.events.pipe(
       filter(event => event instanceof NavigationEnd)
     ).subscribe((event: any) => {
@@ -39,5 +42,25 @@ export class HeaderComponent {
 
   onHome(): void {
     this.router.navigate(['/me']);
+  }
+
+  redirectToLanguage(lang: 'en' | 'hu'): void {
+    window.location.href = `https://omegahouses.org/${lang}`;
+  }
+
+  private detectLangFromUrl(): void {
+    const hostname = window.location.hostname;
+    const pathname = window.location.pathname;
+
+    // Check if URL includes "/en" or "/hu"
+    if (pathname.startsWith('/en')) {
+      this.currentLang = 'en';
+    } else {
+      this.currentLang = 'hu';
+    }
+  }
+
+  showLangButton(lang: 'en' | 'hu'): boolean {
+    return this.isLoginRoute() && this.currentLang !== lang;
   }
 }
