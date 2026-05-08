c6deee5574c73a969af49791c2eda90bad8f6682
Gyula SZABO <gyulaother@gmail.com>
2026-05-09 00:43:38 +0200

    Security: Remove hardcoded API key and use GitHub Secrets for injection

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'https://omegahouses.org/api',
-  apiKeyValid: '59664478-4444-4221-8663-888444222555'
+  apiKeyValid: 'API_KEY_PLACEHOLDER'
 };
